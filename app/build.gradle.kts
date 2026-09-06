6fbb1fa074aac070a975af0da55dd546428d043a
solver492 <digitalsolverland@gmail.com>
2026-09-06 18:32:31 +0100

    feat: add coil-video dependency
    Include the coil-video library to enable support for video frame loading in the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
   implementation(libs.androidx.room.ktx)
   implementation(libs.androidx.room.runtime)
   implementation(libs.coil.compose)
+  implementation(libs.coil.video)
   implementation(libs.converter.moshi)
   implementation(libs.firebase.ai)
   // Uncomment to use Firestore:
